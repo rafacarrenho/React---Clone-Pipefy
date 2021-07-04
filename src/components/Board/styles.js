9c44477d9469c1571848c6835ecb3576e9f981ed
rafael.carrenho <rafael.carrenho@objective.com.br>
2021-07-04 16:16:04 -0300

    Feat: Add readme

--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -1,8 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   padding: 30px 0 0 0;
   height: calc(100% - 114px);
+  min-height: 400px;
   overflow-x: scroll;
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import { Container } from "./styles";
+import { Container } from './styles';
 const Header = () => {
   return (
     <Container>
-      <h1>Clone Pipefy</h1>
+      <h1>Taskboard</h1>
     </Container>
   );
 };
